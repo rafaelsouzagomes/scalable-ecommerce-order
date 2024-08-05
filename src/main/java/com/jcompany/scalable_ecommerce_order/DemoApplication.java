00f3e140e8f697047b7729274bd89d0e45c9fa14
Rafael Gomes <rafaelsouzagomes13@gmail.com>
2024-08-04 23:01:16 -0300

    init

---
+++ src/main/java/com/jcompany/scalable_ecommerce_order/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.jcompany.scalable_ecommerce_order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
